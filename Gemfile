05bf81c5deb3c51586a6668fcea87e913ada31cb
John Michael Fischer <johnmichael.fischer@gmail.com>
2014-09-14 19:49:02 -0400

    succesfully receives messages from spark cloud

--- Gemfile
+++ Gemfile
@@ -1,6 +1,15 @@
+if RUBY_VERSION =~ /1.9/
+Encoding.default_external = Encoding::UTF_8
+Encoding.default_internal = Encoding::UTF_8
+end
+
 source 'https://rubygems.org'
 
 gem 'eventmachine', '>=1.0.3', :git => 'https://github.com/eventmachine/eventmachine.git'
-gem 'em-eventsource'
+
+#gem 'em-eventsource'
+gem 'em-eventsource' , '>=0.2', :git => 'https://github.com/WhiskeyBravo/em-eventsource.git', :branch => 'patch-1'
+
 gem 'em-http-request'
 gem 'multi_json'
+gem 'pry'
--- pullAndPush.rb
+++ pullAndPush.rb
@@ -4,15 +4,25 @@ require 'bundler/setup'
 require 'eventmachine'
 require 'em-eventsource'
 require 'em-http/middleware/json_response'
+require 'pry'
+require 'json'
 
 ACCESS_TOKEN = '94143b4a1dccdcdecfcc540d18a1075e86b78a38'
 
 headers = {'Authorization' => "Bearer #{ACCESS_TOKEN}"}
+#puts headers
 EM.run do
-  source = EventMachine::EventSource.new('https://api.spark.io/v1/devices/53ff75065075535121101687/events', headers)
-  source.use EM::Middleware::JSONResponse
-  source.message do |message|
-    puts "new event: #{message.name} data: #{message.data}"
-  end
-  source.start # Start listening
+    source = EventMachine::EventSource.new('https://api.spark.io/v1/devices/53ff75065075535121101687/events', {}, headers)
+    #source.use EM::Middleware::JSONResponse
+    source.message do |name, message|
+        jsonMsg = JSON.parse(message)
+        puts "new event: #{name} data: #{jsonMsg['data']}"
+    end
+    source.open do
+        puts "opened: #{source.ready_state}"
+    end
+    source.error do |error|
+        puts "ERROR! #{error}"
+    end
+    source.start # Start listening
 end
